81fb6a635f262a6603f738a78e4b034d9fc7ee48
Suhas Manjunath <suhas.manjunath@sailpoint.com>
2024-05-21 13:33:40 +0530

    Initial commit bookzy maven app

---
+++ src/test/java/com/bookzy/maven/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookzy.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
